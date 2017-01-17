45e360bc1cd6de6c2da94addcd8ad8e2bf4141f3
ronggong <ronggong1010@gmail.com>
2017-01-17 19:21:14 +0100

    recalculated score.json, wrong character found

--- phoneticSimilarity/runExp.py
+++ phoneticSimilarity/runExp.py
@@ -7,6 +7,8 @@ lyricsRecognizerPath = path.join(currentPath, 'lyricsRecognizer')
 sys.path.append(lyricsRecognizerPath)
 
 from resultAnalysis import resultAnalysisProcess
+from general.filePath import dnn_node
+from general.parameters import am
 
 methods = ['candidateSynthesizeData',
            'candidateSynthesizeGMM',
@@ -15,15 +17,17 @@ methods = ['candidateSynthesizeData',
            'lyricsRecognizerHSMM']
 
 # # lyricsRecognizer HMM
-# method = methods[3]
-#
-# generalProcess(method,0,am='dnn')
-#
-# resultAnalysisProcess(method,0,am='dnn')
+method = methods[3]
+
+generalProcess(method,0,am=am,dnn_node=dnn_node)
+
+resultAnalysisProcess(method,0,am=am,dnn_node=dnn_node)
 
 # lyricsRecognizer HSMM
-method = methods[4]
+# method = methods[4]
 
-for proportionality_std in [0.1,0.2,0.3,0.5,1.0,2.0,3.0,5.0]: # for the experiment of HSMM
-    generalProcess(method,proportionality_std)
-    resultAnalysisProcess(method,proportionality_std)
+# for proportionality_std in [0.1,0.2,0.3,0.5,1.0,2.0,3.0,5.0]: # for the experiment of HSMM
+# for proportionality_std in [3.0,5.0]: # for the experiment of HSMM
+
+    # generalProcess(method,proportionality_std,am='gmm')
+    # resultAnalysisProcess(method,proportionality_std,am='gmm')
